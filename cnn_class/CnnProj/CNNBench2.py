c46a8d1dc325cc9459231cd2a1d821e80e2d9e65
bart holmberg <bart.holmberg@gmail.com>
2020-10-23 13:40:12 -0700

    daily checkin, categorical class weighting still an issue

--- cnn_class/CnnProj/CNNBench2.py
+++ cnn_class/CnnProj/CNNBench2.py
@@ -19,7 +19,6 @@ import dataAssemble as da
 import matplotlib.pyplot as plt
 
 num_classes = 2 # picasso or not picasso
-
 # this file is the resnet50 model trained on ImageNet data...
 # "notop" means the file does not include weights for the last layer (prediction layer)
 # in order to allow for transfer learning
@@ -59,61 +58,67 @@ from tensorflow.python.keras.applications.resnet50 import preprocess_input
 from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.python.keras.preprocessing.image import image
 image_size = 224
-data_generator_no_aug = ImageDataGenerator(preprocessing_function=preprocess_input, rescale=1.0)
+data_generator_aug = ImageDataGenerator(preprocessing_function=preprocess_input, rescale=1.0,  horizontal_flip=True,vertical_flip=True,rotation_range=180,
+                                   width_shift_range = 0.2,
+                                   height_shift_range = 0.2)
+data_generator_no_aug = ImageDataGenerator(preprocessing_function=preprocess_input)
+
 working_train_dir='D:\\workTrain'
 working_test_dir='D:\\workTest'
 isRefreshWeights=True
 if (isRefreshWeights):
-    train_generator_no_aug = data_generator_no_aug.flow_from_directory(working_train_dir,
+    train_generator_aug = data_generator_aug.flow_from_directory(working_train_dir,
         target_size=(image_size, image_size),
         batch_size=50,
         class_mode='categorical')
-
-    validation_generator = data_generator_no_aug.flow_from_directory(working_test_dir,
+    validation_generator_aug = data_generator_aug.flow_from_directory(working_test_dir,
+        target_size=(image_size, image_size),
+        batch_size=50,
+        class_mode='categorical')
+    validation_generator_no_aug = data_generator_no_aug.flow_from_directory(working_test_dir,
         target_size=(image_size, image_size),
         batch_size=50,
         class_mode='categorical')
     
 model.load_weights("wpicasso.h5")  
+misses=0
+falarm=0
+correct=0
 if not isRefreshWeights:
     model.load_weights("wpicasso.h5")    
 if not isRefreshWeights:
-    idg =data_generator_no_aug.flow_from_directory(directory=working_train_dir,target_size=(image_size, image_size),batch_size=50,class_mode='categorical')
+    idg =data_generator_no_aug.flow_from_directory(directory=working_test_dir,
+       target_size=(image_size, image_size),batch_size=50,class_mode='categorical')
     for imgs in idg:
+      print('false alarm: ',falarm,'misses: ',misses,'correct: ',correct)
       yhat=np.squeeze(model.predict(imgs))
       yhat = np.rint(yhat).astype(int)
       labels =np.array(imgs[1][:].astype(int))
       #img = image.array_to_img(a)
       a=np.squeeze(imgs[0])
-      for i in range(0,49 ):
-          if (labels[i][0] != yhat[i][0]) :
+      for i in range(0,50):
+          if ( labels[i][0] != yhat[i][0] ) :
             b=np.squeeze(a[i,:,:])
-            #xxx = image.array_to_img(a)
-            #xxx = image.img_to_array(a)
- 
             isPicasso=(labels[i][0]==0)
             if isPicasso:
-                b = cv2.putText(b, 'Picasso',  (50, 50) , cv2.FONT_HERSHEY_SIMPLEX,  1, (30, 0, 0) , 4, cv2.LINE_AA) 
+                b = cv2.putText(b, 'Picasso',  (30, 30) , cv2.FONT_ITALIC,  1, (10, 0, 0) , 2, cv2.LINE_AA) 
+                misses=misses+1
             else:
-                b = cv2.putText(b, 'Not Picasso',  (50, 50) , cv2.FONT_HERSHEY_SIMPLEX,  1, (0, 0, 30) , 4, cv2.LINE_AA) 
+                b = cv2.putText(b, 'Not Picasso',  (30, 30) , cv2.FONT_ITALIC,  1, (0, 0, 10) , 2, cv2.LINE_AA) 
+                falarm=falarm+1
             plt.imshow(b)
             plt.show(block=True)
-            plt.draw()  
-
-
-
-
+            plt.draw()
+          else:
+            correct=correct+1
 print("\n\nmodel - train_generator")
-
-
-
 if isRefreshWeights:
     history = model.fit_generator(
-      train_generator_no_aug,
+      train_generator_aug,
       steps_per_epoch=10,
-      epochs=3,
-      class_weight={0:85,1:15},
-      validation_data=validation_generator,
+      epochs=10,
+      #class_weight={0:85,1:15},
+      validation_data=validation_generator_aug,
       validation_steps=3)
     model.save_weights("wpicasso.h5")
 model.summary()
