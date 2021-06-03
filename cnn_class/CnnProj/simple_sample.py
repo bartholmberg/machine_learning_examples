f214d5dff04e8ee7801cdf63e7aaa441136f426a
bart holmberg <bart.holmberg@gmail.com>
2021-06-03 15:41:18 -0700

    minor fixes

--- cnn_class/CnnProj/simple_sample.py
+++ cnn_class/CnnProj/simple_sample.py
@@ -15,7 +15,6 @@ from pyk4a import PyK4A
 from matplotlib import pyplot as plt
 # Add .. to the import path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 import k4a
 
 
@@ -23,17 +22,15 @@ import k4a
 if __name__ == "__main__":
     k4a = PyK4A()
     k4a.start()
-    pcd = o3d.geometry.PointCloud()
-    np_points = np.random.rand(100, 3)
+    #pcd = o3d.geometry.PointCloud()
+    #np_points = np.random.rand(100, 3)
 
-# From numpy to Open3D
-    pcd.points = o3d.utility.Vector3dVector(np_points)
+    #pcd.points = o3d.utility.Vector3dVector(np_points)
 
 # From Open3D to numpy
-    np_points = np.asarray(pcd.points)
+    #np_points = np.asarray(pcd.points)
     capture = k4a.get_capture()
     img_color = capture.color
-    #source_raw=capture.depth_point_cloud
     source_raw=capture.depth_point_cloud
     azPcd = o3d.geometry.PointCloud()
 
